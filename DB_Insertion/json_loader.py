31f4677fd0c18a7dd0857b22b5f8030548bd2714
NaladalaNavya <lanavya.21bce7791@vitapstudent.ac.in>
2025-05-29 11:09:10 +0500

    commit

--- DB_Insertion/json_loader.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def load_mapped_output(file_path):
-    with open(file_path, "r") as f:
-        return json.load(f)
--- Data_collection_agent/main.py
+++
@@ -1,4 +0,0 @@
-from intake_process import run_intake_pipeline
-
-if __name__ == "__main__":
-    run_intake_pipeline()
